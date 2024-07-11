c8cba05e6a50db2afec73729c09b07f309e20317
FelipeVianaFlorentino <felipeviana04@gmail.com>
2024-07-11 00:58:31 -0300

    funcinoalidade Cases Gsap funcionando

--- src/script/components/cases.js
+++ src/script/components/cases.js
@@ -1,25 +1,102 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const casesWidth = document.querySelector('.cases').offsetWidth;
+
+//     function setWidthToScroll() {
+//         widthToScroll = casesWidth - window.innerWidth;
+//     }
+
+
+//     window.addEventListener('resize', setWidthToScroll)
+//     setWidthToScroll();
+
+    
+//     const tlCases = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: '.cases-wrapper',
+//             markers: true,
+//             pin: true,
+//             scrub: true
+//         }
+//     });
+
+//     tlCases.to('.cases', {
+//         x: -widthToScroll
+//     });
+
+//     const spacing = document.querySelector('.spacing');
+//     const spacingHeight = casesWidth - window.innerWidth - window.innerHeight;
+//     spacing.style.height = spacingHeight + 'px';
+
+// })
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    const casesWidth = document.querySelector('.cases').offsetWidth;
+    // team spacing
+    const heightTrigger = document.querySelector('.cases');
+    let howMuchSpacing = heightTrigger.offsetWidth;
 
-    function setWidthToScroll() {
-        widthToScroll = casesWidth - window.innerWidth;
-    }
+    // spacing.style = "height:" + howMuchSpacing/1.5 + "px";
 
-    window.addEventListener('resize', setWidthToScroll)
-    setWidthToScroll();
+    // TEAM OPACITY
 
-    const tlCases = gsap.timeline({
+    const teams = document.querySelector('.case-item');
+
+    const teamsTL = gsap.timeline({
         scrollTrigger: {
-            trigger: '.cases-wrapper',
+            trigger: ".cases-wrapper",
+            start: "top top",
+            end: "bottom top",
             markers: true,
-            pin: true,
             scrub: true
         }
+    }).to(teams, {
+        opacity: 0
     });
 
-    tlCases.to('.cases', {
-        x: -widthToScroll
+
+
+    // TEAM HORIZONTAL
+    const races = document.querySelector('.cases');
+    const teamWrapper = document.querySelector('.cases-wrapper');
+    // let amountToScroll = racesWidth - window.innerWidth;
+
+    function getScrollAmount(){
+        const racesWidth = races.offsetWidth;
+        return -(racesWidth - window.innerWidth);
+    }
+
+
+    const tween = gsap.to(races, {
+        x: getScrollAmount,
+        duration: 3,
+        ease: "none"
     });
 
-})
+
+    ScrollTrigger.create({
+        trigger: ".cases-wrapper",
+        start: "top top",
+        end: () => `+=${getScrollAmount() * -1}`,
+        pin: true,
+        animation: tween,
+        scrub: true,
+        invalidateOnRefresh: true
+        // markers: true
+    })
+
+    function atualizarAlturaFooter(){
+        var team = document.querySelector('.cases');
+        var footerSpacing = team.clientWidth - window.innerWidth;
+        var footer = document.querySelector('.spacing');
+        
+        footer.style.height = footerSpacing + 'px';
+    };
+    
+    window.addEventListener('resize', atualizarAlturaFooter);
+    atualizarAlturaFooter();
+    
+
+
+
+});
