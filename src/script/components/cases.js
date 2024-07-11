042b9cd5967a9ea0d349692e897d8790335c9eff
FelipeVianaFlorentino <felipeviana04@gmail.com>
2024-07-11 00:00:09 -0300

    estrutura do Cases feita

--- src/script/components/cases.js
+++ src/script/components/cases.js
@@ -1,57 +1,25 @@
-// team spacing
-const spacing = document.querySelector('.case-spacing');
-const heightTrigger = document.querySelector('.cases');
-let howMuchSpacing = heightTrigger.offsetWidth;
+document.addEventListener('DOMContentLoaded', () => {
 
-// spacing.style = "height:" + howMuchSpacing/1.5 + "px";
+    const casesWidth = document.querySelector('.cases').offsetWidth;
 
-// TEAM OPACITY
-
-const teams = document.querySelector('.case-block');
-
-const teamsTL = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".case-wrapper",
-        start: "top top",
-        end: "bottom top",
-        // markers: true,
-        scrub: true
+    function setWidthToScroll() {
+        widthToScroll = casesWidth - window.innerWidth;
     }
-}).to(teams, {
-    opacity: 0
-});
 
+    window.addEventListener('resize', setWidthToScroll)
+    setWidthToScroll();
 
+    const tlCases = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.cases-wrapper',
+            markers: true,
+            pin: true,
+            scrub: true
+        }
+    });
 
-// TEAM HORIZONTAL
-const races = document.querySelector('.cases');
-const teamWrapper = document.querySelector('.case-wrapper');
-// let amountToScroll = racesWidth - window.innerWidth;
+    tlCases.to('.cases', {
+        x: -widthToScroll
+    });
 
-function getScrollAmount(){
-    const racesWidth = races.offsetWidth;
-    return -(racesWidth - window.innerWidth);
-}
-
-
-const tween = gsap.to(races, {
-    x: getScrollAmount,
-    duration: 3,
-    ease: "none"
-});
-
-
-ScrollTrigger.create({
-    trigger: ".case-wrapper",
-    start: "top top",
-    end: () => `+=${getScrollAmount() * -1}`,
-    pin: true,
-    animation: tween,
-    scrub: 1,
-    invalidateOnRefresh: true
-    // markers: true
 })
-
-
-
-
